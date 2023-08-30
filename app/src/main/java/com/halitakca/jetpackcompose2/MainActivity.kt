b878f578e59a8db5d2989b4888fe74603a449397
Halit Akca <halitakca070@gmail.com>
2023-08-30 17:24:59 +0300

    Çoklu Dil Desteğimiz. -Adding multi-language support.

--- app/src/main/java/com/halitakca/jetpackcompose2/MainActivity.kt
+++ app/src/main/java/com/halitakca/jetpackcompose2/MainActivity.kt
@@ -1,17 +1,17 @@
 package com.halitakca.jetpackcompose2
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -32,12 +32,17 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
 fun Sayfa(){
-    Column(modifier = Modifier.fillMaxHeight()) {
-        KirmiziKare()
-        Yazi(icerik = "Hello Halit !", yaziBoyutu = 15)
-        MaviKare()
+    Column(modifier = Modifier.fillMaxHeight(),
+    verticalArrangement = Arrangement.SpaceEvenly,
+    horizontalAlignment = Alignment.CenterHorizontally) {
+        //Yazi(icerik = "Hello Halit !", yaziBoyutu = 15)
+        Yazi(icerik = stringResource(id = R.string.textYazi), yaziBoyutu = 24)
+        Button(onClick = { }) {
+            Text(text = stringResource(id = R.string.buttonYazi), fontSize = 24.sp)
+        }
     }
 
 }
@@ -59,7 +64,7 @@ fun Yazi(icerik: String, yaziBoyutu: Int){
     Text(text = icerik, fontSize = yaziBoyutu.sp)
 }
 // Preview'da bu görünür:   Ve bütün Compose'ların en altında olmalı yoksa altında kalan compose yok sayılıyor.
-@Preview(showBackground = true)
+@Preview(showBackground = true, locale = "tr")  //  locale = ".." ile language belirle
 @Composable
 fun DefaultPreview() {
     JetpackCompose2Theme {
