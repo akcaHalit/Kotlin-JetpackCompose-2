f994d5e254274a6eba20d3b4ed5d4efc500402bd
Halit Akca <halitakca070@gmail.com>
2023-08-30 16:35:35 +0300

    Box'lar kullanılmak için hazır.

--- app/src/main/java/com/halitakca/jetpackcompose2/MainActivity.kt
+++ app/src/main/java/com/halitakca/jetpackcompose2/MainActivity.kt
@@ -3,26 +3,30 @@ package com.halitakca.jetpackcompose2
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.halitakca.jetpackcompose2.ui.theme.JetpackCompose2Theme
 
+// Run edince bu görünür:
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             JetpackCompose2Theme {
                 // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    Sayfa()
                 }
             }
         }
@@ -30,14 +34,29 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun Sayfa(){
+    Column {
+        Box(modifier = Modifier
+            .size(80.dp)
+            .background(Color.Red)
+        )
+        Box(modifier = Modifier
+            .size(50.dp)
+            .background(Color.Blue)
+        )
+        Box(modifier = Modifier
+            .size(100.dp)
+            .background(Color.Yellow)
+        )
+    }
 }
 
+
+// Preview'da bu görünür:   Ve bütün Compose'ların en altında olmalı yoksa altında kalan compose yok sayılıyor.
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackCompose2Theme {
-        Greeting("Android")
+        Sayfa()
     }
 }
